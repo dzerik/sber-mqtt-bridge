6c00fb0f5cdb4ed793bea5909bb33ad2b409c62b
Mikhail Berezovskiy <mikhail.berezovskiy@jetbrains.com>
2025-09-10 20:19:48 +0700

    Added check for none values in converter

--- mqtt_sber_gate/rootfs/app/devices/utils/color_converter.py
+++ mqtt_sber_gate/rootfs/app/devices/utils/color_converter.py
@@ -8,9 +8,9 @@ class ColorConverter:
         - V: 0–255 → 100–1000
         """
         # Нормализация значений HA
-        ha_hue = max(0, min(360, ha_hue))         # H: 0–360
-        ha_saturation = max(0, min(100, ha_saturation))  # S: 0–100%
-        ha_brightness = int(max(0, min(255, ha_brightness))) # V: 0–255
+        ha_hue = max(0, min(360, ha_hue if ha_hue is not None else 0))         # H: 0–360
+        ha_saturation = max(0, min(100, ha_saturation if ha_saturation is not None else 0))  # S: 0–100%
+        ha_brightness = int(max(0, min(255, ha_brightness if ha_brightness is not None else 0))) # V: 0–255
 
         # Конвертация в Sber HSV
         sber_hue = ha_hue
@@ -29,9 +29,9 @@ class ColorConverter:
         - V: 100–1000 → 0–255
         """
         # Нормализация значений Sber
-        sber_hue = max(0, min(360, sber_hue))          # H: 0–360
-        sber_saturation = max(0, min(1000, sber_saturation))  # S: 0–1000
-        sber_value = max(100, min(1000, sber_value))     # V: 100–1000
+        sber_hue = max(0, min(360, sber_hue if sber_hue is not None else 0))          # H: 0–360
+        sber_saturation = max(0, min(1000, sber_saturation if sber_saturation is not None else 0))  # S: 0–1000
+        sber_value = max(100, min(1000, sber_value if sber_value is not None else 0))     # V: 100–1000
 
         # Конвертация в HA HSV
         ha_hue = sber_hue
